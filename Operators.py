08571b05ceeab7090d85e671a6374e215d90feee
dikshabhor791 <dikshabhor79@gmail.com>
2025-01-14 22:56:01 +0530

    add Operators file

---
+++ Operators.py
@@ -0,0 +1,64 @@
+print("Arithmatic Operators")
+a = 10
+b = 20
+
+print(" Sum : " , a+b)
+print(" Sub : " , a+b)
+print(" Mul : " , a+b)
+print(" Div : " , a+b)
+print(" Mod : " , a+b)
+
+print("Comparison Operators")
+
+num1=10
+num2=15
+num3=10
+
+print(" Equal : " ,num1 == num2 )               #false
+print(" Equal : " , num1 == num3 )              #true
+print(" Not Equal : " , num1 != num2 )          #true
+print(" Grater than : " , num1 > num2 )         #false
+print(" Lesser than : " , num1 < num2 )         #true
+print(" Grater than Equal : " , num1 >= num2 )  #false
+print(" Lesser than Equal : " , num1 <= num2 )  #true
+
+
+print("Assignment opperators")
+
+val = 10
+
+val += 5
+print("Sum : ",val)     #15
+
+val -= 5
+print("Sub : ",val)     #5
+
+
+val *= 5
+print("Mul : ",val)     #50
+
+
+val /= 5
+print("Div : ",val)     #2
+
+
+val %= 5
+print("Mod : ",val)     #0.0
+
+val **=5       
+print("Power : ",val)   #10^5   wrong answer
+
+print("Logical operators")
+
+val1 = 20
+val2 = 30
+
+print("AND : " , (val1 > val2) and (val1 <= val2))     #false
+print("OR : " ,(val1 > val2) or (val1 <= val2))      #true
+print( "NOT : " ,not (val1 <= val2))                  #false opposite ans of expression
+
+
+
+
+
+
