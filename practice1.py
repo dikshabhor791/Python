88b4dd529bb729d9ba981aa14e06fca7fe5eb521
dikshabhor791 <dikshabhor79@gmail.com>
2025-01-15 22:32:58 +0530

    practice set 1

---
+++ practice1.py
@@ -0,0 +1,29 @@
+# Que 1 = input 2 no and print sum
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+sum = num1 + num2
+print("Sum : " , sum)
+
+print("-------------------------------------------------------------------")
+
+# Que 2 = input side of square and print area
+
+side = int(input("enter the value of side : "))
+area = side * side
+print(area) 
+
+print("-------------------------------------------------------------------")
+
+# Que 3 = input 2 no and print avrage
+
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+print(" avg = " ,( num1 + num2) / 2)
+
+print("-------------------------------------------------------------------")
+# Que 4 = input 2 no and print True if a is greater than b therwise print false
+ 
+a = int(input("enter number :"))
+b = int(input("enter number :"))
+print(a >= b)
