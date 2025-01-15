d578128159fca034c90715e5f8c4f0b0021c0f5f
dikshabhor791 <dikshabhor79@gmail.com>
2025-01-15 21:52:41 +0530

    add type conversion

---
+++ TypeConversion.py
@@ -0,0 +1,22 @@
+print("Type conversion")
+
+a = 10
+b = 4.67
+print(a+b)          #10.0+4.67
+print(type(a))
+print(type(b))
+
+print("-------------------------------------------------------")
+
+print("Type Casting")
+
+c = int("50")
+d = 20
+print(type(c))
+print(c+d)  
+
+
+""" the type conversion is done automatically but the type casting is done
+    manually by the programmer . 1st int is converted into float
+    and in the 2nd we give some instruction  variable = type(value)
+      """
